1db6818625189e98ed7b7e19d95c7ebac27a89d5
Rayyan Sadiq <raysadik5@gmail.com>
2024-04-01 07:43:40 -0500

    commit 6 - learned about spread operator - learned about foreach method

---
+++ JAVASCRIPT/Basics/Arrays/JoiningArrays.js
@@ -0,0 +1,10 @@
+const numbers = [1,2,3]
+const joined = numbers.join(', ') // joins an array into one string with a sequence
+console.log(joined); // print 1, 2, 3
+
+const message = 'This is my first message'
+const parts = message.split(' ')
+console.log(parts);
+
+const combined = parts.join("|")
+console.log(combined)
